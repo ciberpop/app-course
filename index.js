e831603eb32e8d5496c07f5fb77de0f4ca1ff14c
ciberpop <jelrainverner@gmail.com>
2020-05-09 23:56:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cartRoutes = require('./routes/cart')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/cart', cartRoutes)
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+    try {
+        const url = `mongodb+srv://ciberpop:b8xazRtZ1qmrBpX3@cluster0-ulavc.mongodb.net/shop`
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
+
+
+const password = 'b8xazRtZ1qmrBpX3'
