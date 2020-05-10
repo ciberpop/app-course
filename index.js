b13974a226325e0a80bdda0047b82484fddc4851
ciberpop <jelrainverner@gmail.com>
2020-05-10 18:33:22 +0300

    add cart

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require('./routes/home')
 const cartRoutes = require('./routes/cart')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
 const app = express()
 
@@ -17,6 +18,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5eb7f7c21c27cf2da575f9e6')
+        req.user = user
+        next()
+    } catch (e) {
+        console.log(e);
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -36,6 +47,16 @@ async function start() {
             useFindAndModify: false,
             useUnifiedTopology: true
         })
+
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: 'jelil@gmail.com',
+                name: 'Verner',
+                cart: {items: []}
+            })
+            await user.save()
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,20 @@
 const {Router} = require('express')
-const Cart = require('../models/cart')
 const Course = require('../models/course')
 const router = Router()
 
+function mapCartItems(cart) {
+    return cart.items.map(c => ({
+        ...c.courseId._doc, count: c.count
+    }))
+}
+function computePrice(courses) {
+    return courses.reduce((total, course) => {
+        return total += course.price * course.count
+    }, 0)
+}
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id)
-    await Cart.add(course)
+    await req.user.addToCart(course)
     res.redirect('/cart')
 })
 
@@ -15,12 +24,17 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    const cart = await Cart.fetch()
+    const user = await req.user
+        .populate('cart.items.courseId')
+        .execPopulate()
+
+    const courses = mapCartItems(user.cart)
+
     res.render('cart', {
         title: 'Cart',
         isCart: true,
-        courses: cart.courses,
-        price: cart.price
+        courses: courses,
+        price: computePrice(courses)
     })
 })
 
